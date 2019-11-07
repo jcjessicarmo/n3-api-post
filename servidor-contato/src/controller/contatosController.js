f996950b963e9b9b4460ac61eef9c74053bd8f8f
unknown <jessicascarmo@terra.com.br>
2019-11-06 21:36:15 -0300

    Colocar números random

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -6,6 +6,11 @@ const getAll = (request, response) => {
 };
 
 const add = (request, response) => {
+let contato= request.body
+contato.id=Math.random().toString(36).substr(-8)
+
+
+  console.log(contato)
 model.agenda.contatos.push(request.body)
 response.status(200).send()
 }
