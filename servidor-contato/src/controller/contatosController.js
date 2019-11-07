8c25b071dc10a1f6e3eff5ecd1f82d44c0f7d3e6
unknown <jessicascarmo@terra.com.br>
2019-11-06 22:26:32 -0300

    Salvar nomes

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -6,11 +6,25 @@ const getAll = (request, response) => {
 };
 
 const add = (request, response) => {
-let contato= request.body
+let contato = request.body
+let contato = request.body.nome
+
 contato.id=Math.random().toString(36).substr(-8)
+console.log(contato)
 
-
+const nome = (request, response) => {
+  let contato = request.body.nome
+  
+  if (model.agenda.contatos.nome != model.agenda.contatos.nome ) {
+    response.status(200).send()
+    model.agenda.contatos.push(contato)
+  }else{ 
+    response.status(404).send()
+  }
   console.log(contato)
+}
+
+
 model.agenda.contatos.push(request.body)
 response.status(200).send()
 }
