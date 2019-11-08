1c3f9b5873ca3be05152fbea0f02719eb68fc0f7
jcjessicarmo <jessicascarmo@terra.com.br>
2019-11-07 22:14:30 -0300

    Atualizar os cadastro nome

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -7,30 +7,31 @@ const getAll = (request, response) => {
 
 const add = (request, response) => {
 let contato = request.body
-let contato = request.body.nome
+let contatos = request.body.nome
 
 contato.id=Math.random().toString(36).substr(-8)
 console.log(contato)
+}
 
 const nome = (request, response) => {
-  let contato = request.body.nome
+  let contatos = request.body.nome
   
-  if (model.agenda.contatos.nome != model.agenda.contatos.nome ) {
+  if (model.agenda.contatos.nome === request.body.nome)
+    response.status(400).send()
+  else{ 
+    (model.agenda.contatos.nome !== request.body.nome)
     response.status(200).send()
     model.agenda.contatos.push(contato)
-  }else{ 
-    response.status(404).send()
   }
-  console.log(contato)
+  console.log(contatos)
 }
 
-
-model.agenda.contatos.push(request.body)
-response.status(200).send()
-}
+// {
+// model.agenda.contatos.push(request.body)
+// response.status(200).send()
+// }
 
 module.exports = {
   getAll,
-  add
+  add, 
 }
-
